5a6b173ff6e7c6d595f46821bb02de68c9e2bee2
amisu <hamisurahman@gmail.com>
2024-01-17 12:00:40 +0000

    Home page btn

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import Header from "./components/Header";
 import PostPage from './pages/PostPage'
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Container maxW="620px">
       <Header />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/post/:pid" element={<PostPage />} />
       </Routes>
